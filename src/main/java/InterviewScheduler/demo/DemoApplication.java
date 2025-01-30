e51f6c59eaa941a00e2fd51969bb0440569632ad
Nandini Jatania <nandini.jatania@ripplehire.com>
2025-01-30 13:01:30 +0530

    Spring Initializer Class

--- src/main/java/InterviewScheduler/demo/DemoApplication.java
+++ src/main/java/InterviewScheduler/demo/DemoApplication.java
@@ -2,15 +2,20 @@ package InterviewScheduler.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
-public class DemoApplication {
+@EnableAsync  // Leave it if you plan to use async methods
+public class DemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
